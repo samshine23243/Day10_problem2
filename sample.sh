0d0d36ef7eca1a2ef0c81359dadad42d063c333e
samshine <sameershinde23243@gmail.com>
2021-02-18 10:28:36 +0530

    use case 3 and 4 code added

--- sample.sh
+++ sample.sh
@@ -1,8 +1,8 @@
-echo  "flip coin simulation problem"
-head=1
-tail=0
-randomNumber=$(($RANDOM%2))
-if [ $randomNumber -eq 1 ]
+echo "flip coin simulation problem" 
+head=1 
+tail=0 
+randomNumber=$(($RANDOM%2)) 
+if [ $randomNumber -eq 1 ] 
 then
 	echo "head winner"
 else
@@ -10,7 +10,8 @@ else
 fi
 headcount=0
 tailcount=0
-for ((i=1;i<=10;i++))
+
+for ((i=1;i<=20;i++))
 do
 	randomNumber=$(($RANDOM%2))
 		if [ $randomNumber -eq 1 ]
@@ -20,5 +21,33 @@ do
    		tailcount=$(($tailcount+1))
 		fi
 done
-echo "head won count:" $headcount
-echo "tail won count:" $tailcount
+
+if [ $headcount -gt $tailcount ]
+then
+	diff=$(($headcount-$tailcount))
+	echo "head won by difference:" $diff
+elif [ $headcount -lt $tailcount ]
+then
+	diff=$(($tailcount-$headcount))
+	echo "tail won by difference:" $diff
+else
+	echo "its tie"
+	for ((i=1;i<=3;i++))
+	do
+		randomNumber=$(($RANDOM%2))
+		if [ $randomNumber -eq 1 ]
+		then
+			headcount=$(($headcount+1))
+		else
+			tailcount=$(($tailcount+1))
+		fi
+	done
+
+	if [ $headcount -gt $tailcount ]
+	then
+		echo "head won..."
+	else
+		echo "tail won..."
+	fi
+fi
+
