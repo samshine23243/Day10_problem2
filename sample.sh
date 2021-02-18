006e505722acac2d1e054e7d2a7758c696d285c9
samshine <sameershinde23243@gmail.com>
2021-02-18 09:51:29 +0530

    use case 2 code added

--- sample.sh
+++ sample.sh
@@ -8,3 +8,17 @@ then
 else
 	echo "tail winner"
 fi
+headcount=0
+tailcount=0
+for ((i=1;i<=10;i++))
+do
+	randomNumber=$(($RANDOM%2))
+		if [ $randomNumber -eq 1 ]
+		then
+   		headcount=$(($headcount+1))
+		else
+   		tailcount=$(($tailcount+1))
+		fi
+done
+echo "head won count:" $headcount
+echo "tail won count:" $tailcount
